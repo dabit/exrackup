8b7eae5aa9336f688fea82310614bdf01ca60515
David Padilla <david@padilla.cc>
2016-07-01 13:22:52 -0500

    Handle concurrent connections

--- lib/exrackup.ex
+++ lib/exrackup.ex
@@ -1,8 +1,21 @@
 defmodule Exrackup do
+  use Application
   use Export.Ruby
 
   def call do
     {:ok, ruby} = Ruby.start(ruby_lib: "./")
     ruby |> Ruby.call("lib/server", "call_that_method", [{}])
+  end
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      supervisor(Task.Supervisor, [[name: Server.TaskSupervisor]]),
+      worker(Task, [Server, :accept, [4001]])
+    ]
+
+    opts = [strategy: :one_for_one, name: Server.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
